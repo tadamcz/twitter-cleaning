2db7cc1d1a004addd3032bad18067469e8c1616e
Tom Adamczewski <tmkadamcz@gmail.com>
2023-04-26 19:28:12 +0000

    print tweet count

--- tweets.js
+++ tweets.js
@@ -2,7 +2,10 @@ const { twitter, treshold, sleep } = require('./config')
 const { ApiResponseError } = require('twitter-api-v2')
 
 const main = async () => {
-  const user = await twitter.v2.me()
+  const user = await twitter.v2.me({'user.fields': 'public_metrics'})
+  const tweet_count = user.data.public_metrics.tweet_count
+  console.log(`Tweet count: ${tweet_count}`)
+
   const tweets = await twitter.v2.userTimeline(user.data.id, { 'tweet.fields': 'created_at' })
 
   for await (const tweet of tweets) {
